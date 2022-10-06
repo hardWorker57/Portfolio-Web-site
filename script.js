4b036b0fd0da558269f1d8980543d98164f749bd
Hardworker <pobedovataj@gmail.com>
2022-10-06 18:00:30 +0600

    branch-hover-animations

---
+++ script.js
@@ -0,0 +1,10 @@
+// let question = +prompt("how many family gays did u see there?");
+
+// for (let i = 1; i < question; i++) {
+//     if (question < 7) {
+//         alert("tu est gay")
+//     }
+//     else {
+//         alert("fuck u ,tu achete de du etudiants!")
+//     }
+// }
